486cc4326dc3192f7db0330586d978d4be2219fb
Myke Amorim <mykeleony147@gmail.com>
2021-09-17 00:40:53 -0300

    Create notacoes.c

---
+++ aed/notacoes.c
@@ -0,0 +1,15 @@
+/* Notações importantes da apostila de Algoritmos e Estruturas de Dados 1 do curso de Sistemas de Informação da USP.
+   10 de setembro de 2021.
+   Myke Leony dos Santos Amorim - Bacharelado em Sistemas de Informação - EACH USP.
+*/ 
+
+// Tamanho máximo do valor estatico:
+#define MAX 50
+
+// Definição do tipo chave utilizando:
+typedef int TIPOCHAVE;
+
+typedef struct {
+	TIPOCHAVE chave;
+	// Futuros novos campos...
+} REGISTRO
