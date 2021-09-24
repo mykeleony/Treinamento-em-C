614ba86eaa0214d213a5540417072e3a544e5bb9
Myke Amorim <mykeleony147@gmail.com>
2021-09-24 02:59:45 -0300

    Inserção da função de último elemento
    Além disso, as demais funções foram complementadas.

--- aed/listas_lineares_encadeadas_estaticas.c
+++ aed/listas_lineares_encadeadas_estaticas.c
@@ -24,7 +24,7 @@ void inicializarListaEncadeadaEstatica(LISTA *lista) {
   lista->inicio = -1;
   lista->dispo = 0;
 
-  for(int i = 0; i < MAX-1; i++)
+  for (int i = 0; i < MAX-1; i++)
     lista->A[i].prox = i+1;
 
   lista->A[MAX-1].prox = -1;
@@ -32,6 +32,11 @@ void inicializarListaEncadeadaEstatica(LISTA *lista) {
 
 // Imprime todos os elementos de uma determinada lista linear encadeada:
 void exibirListaEncadeada(LISTA lista) {
+  if (lista.inicio == -1) {
+    printf("Lista vazia.\n");
+    return;
+  }
+
   int i = lista.inicio;
 
   while (i > -1) {
@@ -40,7 +45,6 @@ void exibirListaEncadeada(LISTA lista) {
   }
 
   printf("\n");
-
 }
 
 // Retorna o número de elementos de uma dada lista linear encadeada:
@@ -57,9 +61,27 @@ int tamanhoListaEncadeada(LISTA lista) {
 }
 
 // Retorna a chave do primeiro elemento de uma lista linear encadeada:
-int chavePrimeiroElementoListaEncadeada(TIPOCHAVE chave, LISTA lista) {
+TIPOCHAVE chavePrimeiroElementoListaEncadeada(LISTA lista) {
   if (lista.inicio > -1)
     return lista.A[lista.inicio];
+
+  printf("Não há elementos nesta lista.\n");
+}
+
+// Retorna a chave do último elemento de uma lista linear encadeada:
+TIPOCHAVE chaveUltimoElementoListaEncadeada(LISTA lista) {
+  if(lista.inicio > -1) {
+    int i = lista.inicio;
+
+    while(i > -1) {
+      if(lista.A[i].prox == -1)
+        return lista.A[i].chave;
+
+      i = lista.A[i].prox;
+    }
+  }
+
+  printf("Não há elementos nesta lista.\n")
 }
 
 int main() {
