2332551b9b93e584f31014a27cfe31c1f6c57355
Myke Amorim <mykeleony147@gmail.com>
2021-09-24 02:16:01 -0300

    Inserção da função de inicialização de listas.

--- aed/listas_lineares_encadeadas_estaticas.c
+++ aed/listas_lineares_encadeadas_estaticas.c
@@ -10,11 +10,26 @@ typedef int TIPOCHAVE;
 
 typedef struct {
   TIPOCHAVE chave;
-  int prox;         // Indica o índice do próximo elemento da lista linear encadeada.
+  int prox;         // Índice do próximo elemento da lista linear encadeada.
 } REGISTRO;
 
 typedef struct {
   REGISTRO A[MAX];
-  int inicio;      // Índice do primeiro elemento da lista linear encadeadaa. 
+  int inicio;      // Índice do primeiro elemento da lista linear encadeadaa.
   int dispo;       // Índice do primeiro elemento da lista livre, de espaços disponíveis na lista linear.
 } LISTA;
+
+// Inicialização:
+void inicializarListaEncadeadaEstatica(LISTA *lista) {
+  lista->inicio = -1;
+  lista->dispo = 0;
+
+  for(int i = 0; i < MAX-1; i++)
+    lista->A[i].prox = i+1;
+
+  lista->A[MAX-1].prox = -1;
+}
+
+int main() {
+
+}
