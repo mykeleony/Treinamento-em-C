962e706ecaf2a3b8922270c98403a7cecabb8521
Myke Amorim <mykeleony147@gmail.com>
2021-09-30 23:50:09 -0300

    Create listas_lineares_ligadas_dinamicas.c

---
+++ aed/listas_lineares_ligadas_dinamicas.c
@@ -0,0 +1,11 @@
+/* Programa que realiza operações elementares em listas lineares encadeadas com alocação dinâmica.
+   21 de setembro de 2021.
+   Myke Leony dos Santos Amorim - Bacharelado em Sistemas de Informação - EACH USP.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main() {
+}
