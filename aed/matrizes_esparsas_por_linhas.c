944a11fec649d934f66eed5b1081a61f3f336e4e
Myke Amorim <mykeleony147@gmail.com>
2021-11-11 14:51:38 -0300

    Update and rename matrizes_esparsas.c to matrizes_esparsas_por_linhas.c

---
+++ aed/matrizes_esparsas_por_linhas.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef int TIPOINFO;
+
+typedef struct estrutura {
+	int linha;	// Os nós são organizados pela linha, depois pela coluna.
+	int coluna;
+	TIPOINFO info;
+	struct estrutura *prox;
+} NO;
+
+typedef struct {
+	NO* inicio;
+} MATRIZ;
+
+// Retorna se a matriz esparsa é a melhor escolha de estrutura em uma determinada situação:
+bool matrizEsparsaEficiente (int max_linhas, int max_colunas, int elementos_nao_nulos) {
+	int t_matriz_tradicional = max_linhas * max_colunas * sizeof(TIPOINFO);	// Memória ocupada por uma matriz tradicional
+	int t_matriz_linhas = elementos_nao_nulos*(2*sizeof(int) + sizeof(TIPOINFO) + sizeof(struct estrutura*));
+
+	printf("%d\n", t_matriz_tradicional);
+	printf("%d\n", t_matriz_linhas);
+
+	if (t_matriz_tradicional < t_matriz_linhas || t_matriz_tradicional == t_matriz_linhas)
+		return false;
+
+	return true;
+}
+
+void main() {
+}
