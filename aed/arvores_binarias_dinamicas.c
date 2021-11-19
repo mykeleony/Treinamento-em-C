d1862e80120ee23269f0dbaa77f1115cd380e036
Myke Amorim <mykeleony147@gmail.com>
2021-11-19 03:45:12 -0300

    Adição da rotina de inserção de nó.

--- aed/arvores_binarias_dinamicas.c
+++ aed/arvores_binarias_dinamicas.c
@@ -5,8 +5,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef int TIPOCHAVE;
+
+typedef enum {esq, dir} LADO; // Numeração dos filhos esquerdo e direito de um nó.
 
 typedef struct estrutura {
   TIPOCHAVE chave;
@@ -17,4 +20,38 @@ typedef struct estrutura {
 // Cria uma árvore binária dinâmica vazia:
 void inicializaArvore (NO* *raiz) {
   *raiz = NULL;
+}
+
+// Informa se uma árvore está vazia:
+bool arvoreVazia (NO* raiz) {
+  if (!raiz)
+    return true;
+
+  return false;
+}
+
+// Insere um nó em uma árvore binária comum (sem ordenação):
+bool inserirNo (NO* *raiz, NO* pai, TIPOCHAVE chave, LADO posicao) {
+  if (pai && (posicao == esq && pai->esq || posicao == dir && pai->dir)) // Caso a posição a inserir já esteja ocupada, não é possível inserir.
+    return false;
+
+  NO* elemento = (NO*) malloc(sizeof(NO));
+
+  elemento->chave = chave;
+  elemento->esq = NULL;
+  elemento->dir = NULL;
+
+  if (!pai)  // Inserindo uma nova raiz.
+    *raiz = elemento;
+
+  else if (posicao == esq)
+    pai->esq = elemento;
+
+  else
+    pai->dir = elemento;
+
+  return true;
+}
+
+void main() {
 }
