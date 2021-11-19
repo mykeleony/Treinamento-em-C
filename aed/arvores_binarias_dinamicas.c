fc20ecf987ad4edc0e66cf3cada3c045ac4458f3
Myke Amorim <mykeleony147@gmail.com>
2021-11-19 03:48:46 -0300

    Correção da documentação.

--- aed/arvores_binarias_dinamicas.c
+++ aed/arvores_binarias_dinamicas.c
@@ -41,7 +41,7 @@ bool inserirNo (NO* *raiz, NO* pai, TIPOCHAVE chave, LADO posicao) {
   elemento->esq = NULL;
   elemento->dir = NULL;
 
-  if (!pai)  // Inserindo uma nova raiz.
+  if (!pai)  // Inserindo uma nova raiz ou atualiza a antiga.
     *raiz = elemento;
 
   else if (posicao == esq)
